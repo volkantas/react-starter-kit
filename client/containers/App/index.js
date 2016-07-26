dc0b09cb0c20f39c434af2aed169b12ad11fb9e6
Volkan <volkan@nomadcommerce.com>
2016-07-26 14:08:52 +0300

    initial commit

---
+++ client/containers/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import style from './style.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className={style.normal}>
+        test ediyoruasdasdasz.
+      </div>
+    )
+  }
+}
+
+export default App;
